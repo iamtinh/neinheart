e5d9379cd85095ac83f29b239e3b5d55b820f2f1
iamtinh <46273174+iamtinh@users.noreply.github.com>
2018-12-31 19:52:27 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
 
 bot = commands.Bot(prefix_char='#')
-token = ''
+token = 'NTI5MTQwNDA2NjQyODAyNzE5.DwshGA.sbD1HzSDR_weliLs-Tg2kpmwwU8'
 
 @bot.event
 asyn def on_ready():
