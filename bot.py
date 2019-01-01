96a78d437552125fd4535b886cbd66ff02e8ed4c
iamtinh <46273174+iamtinh@users.noreply.github.com>
2018-12-31 19:56:22 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ bot = commands.Bot(prefix_char='#')
 token = 'NTI5MTQwNDA2NjQyODAyNzE5.DwshGA.sbD1HzSDR_weliLs-Tg2kpmwwU8'
 
 @bot.event
-asyn def on_ready():
+async def on_ready():
  print('Logged as:'+bot.user.name+' '+bot.user.id)
 
 bot.run(token)
