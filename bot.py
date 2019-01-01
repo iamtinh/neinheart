1e0bd122d513684bd6ca18af43d434c683a75461
iamtinh <46273174+iamtinh@users.noreply.github.com>
2018-12-31 19:57:50 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from discord.ext import commands
 
-bot = commands.Bot(prefix_char='#')
+bot = commands.Bot(command_prefix='#')
 token = 'NTI5MTQwNDA2NjQyODAyNzE5.DwshGA.sbD1HzSDR_weliLs-Tg2kpmwwU8'
 
 @bot.event
