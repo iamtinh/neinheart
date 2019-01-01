ad6dd23fcdcbab2a7040a33393841e95faabaff0
iamtinh <46273174+iamtinh@users.noreply.github.com>
2018-12-31 20:07:00 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,4 +7,12 @@ token = 'NTI5MTQwNDA2NjQyODAyNzE5.DwshGA.sbD1HzSDR_weliLs-Tg2kpmwwU8'
 async def on_ready():
  print('Logged as:'+bot.user.name+' '+bot.user.id)
 
+@bot.command()
+async def ping(ctx):
+ embed = discord.Embed(
+  title ='stop'
+ )
+ embed.set_image(url='https://media3.giphy.com/media/l4Ki2obCyAQS5WhFe/giphy.gif')
+ await ctx.send(embed = embed)
+
 bot.run(token)
